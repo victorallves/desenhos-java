3cd4c2dcb98950fe954dba1c1fd9fb90b29af7a8
victor augusto <113953361+victorallves@users.noreply.github.com>
2022-11-23 17:33:15 -0300

    desenhando a letra H.

---
+++ ex2.java
@@ -0,0 +1,23 @@
+package trabalho;
+
+public class exercicio2 {
+
+	public static void main(String[] args) {
+		int[][] matriz = new int [10][10];
+		//j < 3 j > 6
+		
+		for(int i = 0; i < matriz.length ; i ++ ) {	
+			System.out.println("");
+			for(int j = 0; j < matriz.length; j++) {
+				if((j > 2 && j < 7  ) && (i < 3 || i > 6)) {
+					System.out.print("O ");
+				}else {
+					System.out.print("- ");
+				}
+				
+			}
+		}
+				
+	} 
+	
+}
